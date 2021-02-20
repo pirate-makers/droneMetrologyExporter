d7e00ba714f4a66c91a67ccdea804caf9e92893c
prune <prune@lecentre.net>
2021-02-19 20:56:44 -0500

    lots of improvements

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/pirate-makers/droneMetrologyExporter
+
+go 1.14
+
+require github.com/namsral/flag v1.7.4-pre
--- main.go
+++ main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"html/template"
 	"io/ioutil"
+	"math"
+	"os"
 	"regexp"
 	"strconv"
 	"strings"
 	"time"
+
+	"github.com/namsral/flag"
 )
 
 // Read a subtitle file (SRT) with DJI Mini2 Drone telemetry and decode it
@@ -35,11 +41,33 @@ type MetrologySample struct {
 	Zoom            int
 	Latitude        float64
 	Longitude       float64
-	Direction       int
+	Sources         int     // number of connected satellite
+	Bearing         float64 // Direction in degree (0 = north)
 	DTH             float64 // distance to home in meters
 	Altitude        float64 // in meters
 	HorizontalSpeed float64 // in meters/seconds
 	VerticalSpeed   float64 // in meters/seconds
+}
+
+// Represents a Physical Point in geographic notation [lat, lng].
+type Point struct {
+	lat float64
+	lng float64
+}
+
+func BearingTo(p1, p2 *Point) float64 {
+
+	dLon := (p2.lng - p1.lng) * math.Pi / 180.0
+
+	lat1 := p1.lat * math.Pi / 180.0
+	lat2 := p2.lat * math.Pi / 180.0
+
+	y := math.Sin(dLon) * math.Cos(lat2)
+	x := math.Cos(lat1)*math.Sin(lat2) -
+		math.Sin(lat1)*math.Cos(lat2)*math.Cos(dLon)
+	brng := math.Atan2(y, x) * 180.0 / math.Pi
+
+	return brng
 }
 
 // parse SRT file.
@@ -54,13 +82,16 @@ func parseSRT(b []byte) Metrology {
 	r2 := regexp.MustCompile("([0-9:.,]*) --> ([0-9:.,]*)")
 	r3 := regexp.MustCompile("F/([0-9.]*), SS ([0-9.]*), ISO ([0-9.]*), EV ([0-9.-]*), DZOOM ([0-9.]*), GPS \\(([0-9.-]*), ([0-9.-]*), ([0-9.-]*)\\), D ([0-9.-]*)m, H ([0-9.-]*)m, H\\.S ([0-9.-]*)m/s. V\\.S ([0-9.-]*)m/s")
 
+	data := &MetrologySample{}
+	dataLen := 0
+
+	var err error
+
 	for c, l := range lines {
 		if l == "" {
+			data = &MetrologySample{}
 			continue
 		}
-
-		data := &MetrologySample{}
-		var err error
 
 		idMatches := r1.FindStringSubmatch(l)
 		if len(idMatches) == 2 {
@@ -97,15 +128,28 @@ func parseSRT(b []byte) Metrology {
 			data.ISO, _ = strconv.Atoi(dataMatches[3])
 			data.EV, _ = strconv.ParseFloat(dataMatches[4], 64)
 			data.Zoom, _ = strconv.Atoi(dataMatches[5])
-			data.Latitude, _ = strconv.ParseFloat(dataMatches[6], 64)
-			data.Longitude, _ = strconv.ParseFloat(dataMatches[7], 64)
-			data.Direction, _ = strconv.Atoi(dataMatches[8])
+			data.Longitude, _ = strconv.ParseFloat(dataMatches[6], 64)
+			data.Latitude, _ = strconv.ParseFloat(dataMatches[7], 64)
+			data.Sources, _ = strconv.Atoi(dataMatches[8])
 			data.DTH, _ = strconv.ParseFloat(dataMatches[9], 64)
 			data.Altitude, _ = strconv.ParseFloat(dataMatches[10], 64)
 			data.HorizontalSpeed, _ = strconv.ParseFloat(dataMatches[11], 64)
 			data.VerticalSpeed, _ = strconv.ParseFloat(dataMatches[11], 64)
 
+			// compute Heading (Bearing)
+			if dataLen > 1 {
+				bearing := BearingTo(
+					&Point{metrology[dataLen-1].Latitude, metrology[dataLen-1].Longitude},
+					&Point{data.Latitude, data.Longitude},
+				)
+				if bearing == 0 || bearing == 180 {
+					data.Bearing = metrology[dataLen-1].Bearing
+				} else {
+					data.Bearing = bearing
+				}
+			}
 			metrology = append(metrology, data)
+			dataLen++
 
 			continue
 		}
@@ -115,14 +159,107 @@ func parseSRT(b []byte) Metrology {
 	return metrology
 }
 
+func multiply(a, b int) int { return a * b }
+
+// fusionExporter print the metroloy in a format usable as Resolve Fusion objects
+func fusionExporter(m Metrology) {
+	// 	text = comp:TextPlus()
+	// text.StyledText = "Hello World"
+	// text.Center = comp:Path()
+	// text.Center[0] = {-0.5, 0.5, 0.0}
+	// text.Center[60] = {0.5, 0.5, 0.0}
+	// text.Center[120] = {0, 0.5, 0.0}
+	// text.Center[180] = {0.5, 0.5, 0.0}
+	funcMap := template.FuncMap{"multiply": multiply}
+
+	settingsTemplate := `{
+	Tools = ordered() {
+		Drone = RectangleMask {
+			CtrlWZoom = false,
+			Inputs = {
+				Filter = Input { Value = FuID { "Fast Gaussian" }, },
+				MaskWidth = Input { Value = 2016, },
+				MaskHeight = Input { Value = 1222, },
+				PixelAspect = Input { Value = { 1, 1 }, },
+				UseFrameFormatSettings = Input { Value = 1, },
+				ClippingMode = Input { Value = FuID { "None" }, },
+				Width = Input {
+					SourceOp = "DroneWidth",
+					Source = "Value",
+				},
+				Height = Input {
+					SourceOp = "DroneHeight",
+					Source = "Value",
+				},
+			},
+			ViewInfo = OperatorInfo { Pos = { 434, 86.1515 } },
+		},
+		DroneWidth = BezierSpline {
+			SplineColor = { Red = 225, Green = 255, Blue = 0 },
+			NameSet = true,
+			KeyFrames = { 
+			{{ range . -}}
+				[{{ multiply .ID 30 }}] = { {{.Altitude}}, LH = { 20, 0.666666666666667 }, RH = { 40, 0.666666666666667 }, Flags = { Linear = true } },
+			{{ end }}
+			}
+		},
+		DroneHeight = BezierSpline {
+			SplineColor = { Red = 0, Green = 255, Blue = 255 },
+			NameSet = true,
+			KeyFrames = {
+			{{ range . -}}
+				[{{ multiply .ID 30 }}] = { {{ .Bearing }}, LH = { 20, 0.666666666666667 }, RH = { 40, 0.666666666666667 }, Flags = { Linear = true } },
+				{{ end }}
+			}
+		}
+	}
+}
+`
+
+	t, err := template.New("settings").Funcs(funcMap).Parse(settingsTemplate)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	err = t.Execute(os.Stdout, m)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	// fmt.Printf("drone = comp:RectangleMask()")
+	// for _, s := range m {
+	// 	fmt.Printf("drone.Width[%d] = %f;\n", s.ID, s.Altitude)
+	// 	fmt.Printf("drone.Height[%d] = %d;\n", s.ID, s.Direction)
+	// }
+}
+
+func jsonExporter(m Metrology) {
+	data, _ := json.MarshalIndent(m, "", "\t")
+	fmt.Println(string(data))
+}
+
+var (
+	// version is filled by -ldflags  at compile time
+	version = "no version set"
+	srtFile = flag.String("srtfile", "sample.srt", "The SRT file containing the metrology")
+	format  = flag.String("format", "json", "output format, json or fusion")
+)
+
 func main() {
-	data, err := ioutil.ReadFile("sample.srt")
+	flag.Parse()
+
+	data, err := ioutil.ReadFile(*srtFile)
 	if err != nil {
 		fmt.Println(err)
 	}
 
 	metrologyData := parseSRT(data)
-	fmt.Println(metrologyData)
+
+	if *format == "json" {
+		jsonExporter(metrologyData)
+	} else {
+		fusionExporter(metrologyData)
+	}
 }
 
 // parseSrtTime parses a srt subtitle time (duration since start of film).
